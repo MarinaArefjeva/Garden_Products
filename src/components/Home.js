8d128c1696c21ea4fc443aaa4b3c6f782023773c
Marina <marina.scekocihina@gmail.com>
2024-01-07 15:09:52 +0100

    Header

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,30 +1,12 @@
 import React from "react";
+import Header from "./Header/Header";
+import Discount from "./HomeModules/Discount/Discount";
 
 export default function Home() {
   return (
-    <header>
-      <div className="logo">
-        <img src="./logo.svg" />
-      </div>
-      <div>
-        <ul className="nav">
-          <li>Main Page</li>
-          <li>Categories</li>
-          <li>All products</li>
-          <li>All sales</li>
-        </ul>
-      </div>
-
-      <div className="icon">
-        <img src="./assets/images/bag.svg" />
-      </div>
-
-      <main>
-        <div className="Text">
-          <h1>Amazing Discounts</h1>
-          <h2>on Garden Products!</h2>
-        </div>
-      </main>
-    </header>
+    <>
+      <Header />
+      <Discount />
+    </>
   );
 }
---
+++ src/components/HomeModules/Discount/Discount.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./Discount.module.css";
+
+export default function Discount() {
+  return (
+    <div className={styles.text}>
+      Amazing Discounts <br /> on Garden Products!
+    </div>
+  );
+}
